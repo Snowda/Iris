c02d40b00802a0519b26ab8123b19a126e89ab46
Conor Forde <sprkwear@gmail.com>
2013-09-02 16:22:45 -0700

    Update iris_checker.py

--- tests/iris_checker.py
+++ tests/iris_checker.py
@@ -1,1 +1,18 @@
-"""applies pylint to iris.py"""
+"""Runs pylint on iris.py with desired attributes"""
+import subprocess, os
+
+tested_file = "iris"
+tested_file_python = "../bin/"+tested_file+".py"
+subprocess.call("pyreverse --project="+tested_file+"UML --filter-mode=ALL "+tested_file_python, 
+    shell=True)
+
+subprocess.call("dot -Tpng classes_"+tested_file+"UML.dot -o "+tested_file+"UML.png", shell=True)
+os.remove("classes_"+tested_file+"UML.dot")
+os.rename(tested_file+"UML.png", "../uml/"+tested_file+"_uml.png")
+
+subprocess.call("pylint --const-rgx='[a-z_][a-z0-9_]{2,30}$'" \
+    " --disable=RP0401 --disable=RP0001 --disable=RP0002 " \
+    "--disable=RP0101 --disable=RP0701 --disable=RP0801 "+tested_file_python, shell=True)
+
+subprocess.call("coverage run "+tested_file_python, shell=True)
+subprocess.call("coverage report", shell=True)
