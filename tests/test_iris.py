767e2cf3d20b2bd1b74d553fb54b672987a4aa26
Conor Forde <sprkwear@gmail.com>
2013-09-02 16:20:19 -0700

    Update test_iris.py

--- tests/test_iris.py
+++ tests/test_iris.py
@@ -1,1 +1,21 @@
-"""checker file for iris"""
+"""unit tests for iris"""
+import unittest
+class TestIris(unittest.TestCase):
+    def testInsufficientArgs(self):
+        foo = 0
+        self.failUnlessRaises(ValueError, Sensor, foo)
+
+    def testchange_cal_settings_args(self):
+        foo = 0
+        self.failUnlessRaises(ValueError, MyClass, foo)
+
+        try:
+            self.assertRaises(myExcTwo,self.myClass.getName)
+        except Exception as e:
+            pass
+
+    def test_span_offset_args(self):
+    
+    def test_zero_offset_args(self):
+    
+    def test_bit_offset_args(self):
